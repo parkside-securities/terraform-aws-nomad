42c22f07cbd763829ef9bfa8198a87143c057dd0
Jonathan Morris <jonathan@parkside.app>
2019-04-24 13:27:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "aws_ami" "nomad_consul" {
   most_recent = true
 
   # If we change the AWS Account in which test are run, update this value.
-  owners = ["marketplace"]
+  owners = ["aws-marketplace"]
 
   filter {
     name   = "virtualization-type"
